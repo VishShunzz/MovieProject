05d5ed3f4bc0d38ed80e41f46d406f67fc192fde
vishal.verma <vishal.verma1@appinventiv.com>
2024-09-07 19:07:16 +0530

    Initial Commit

---
+++ Movies_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies_Project
+//
+//  Created by Vishal  on 07/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movies_Project/Movies_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_ProjectApp.swift
+//  Movies_Project
+//
+//  Created by Vishal  on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Movies_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
